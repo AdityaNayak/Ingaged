00c5908ce8502ac811b40b5fa86ac2f83f7ab358
Rishabh Verma <me@rishabhverma.me>
2013-10-28 15:13:29 +0530

    added a parameter to 'abort_error' to change the http code of error raised.

--- ig_api/error_codes.py
+++ ig_api/error_codes.py
@@ -2,13 +2,17 @@
 """All the error codes with a human readable message are listed here."""
 from flask.ext.restful import abort
 
-def abort_error(error_code):
+def abort_error(error_code, http_code=400):
     """This is used to abort a request in-case of some problem, error etc.
     
-    Note: Right now the HTTP status code for every aborted request will be 400.
-          Error will be recognized by the unique error codes.
+    Keyword Arguments:
+    error_code -- code of the error. should exist in `ig_api.error_codes.ERROR_CODES`
+    http_code -- http_code of the error. defaults to 400.
+
+    Note: We will be keeping HTTP status code for every error produced as 400 (except 404
+          not found). The errors will be recognized by the unique error codes.
     """
-    abort(400, error=True, message=ERROR_CODES[str(error_code)], code=error_code)
+    abort(http_code, error=True, message=ERROR_CODES[str(error_code)], code=error_code)
 
 
 ERROR_CODES = {
