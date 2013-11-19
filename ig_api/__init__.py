640e02f60ceb4b36fab6a0c7102cc14ab41022c2
Rishabh Verma <me@rishabhverma.me>
2013-11-20 02:41:53 +0530

    added content-type header in CORS headers

--- ig_api/__init__.py
+++ ig_api/__init__.py
@@ -62,7 +62,7 @@ api = FlaskRestfulAPI(app)
 def add_access_control_headers(response):
     """Adds the required access control headers"""
     response.headers['Access-Control-Allow-Origin'] = '*'
-    response.headers['Access-Control-Allow-Headers'] = 'Authorization'
+    response.headers['Access-Control-Allow-Headers'] = 'Authorization,Content-Type'
     response.headers['Access-Control-Allow-Methods'] = 'POST,GET,PUT,DELETE'
     response.headers['Cache-Control'] = 'No-Cache'
     return response
