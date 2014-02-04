5497142b5a2cb70b6005ff6ec1c40d281f017bd2
Aditya Nayak <aditya@adityanayak.com>
2014-02-04 11:29:31 +0530

    [Dashboard] Added frontend elements for #65

--- frontend/dashboard/js/dashboard.backbone.js
+++ frontend/dashboard/js/dashboard.backbone.js
@@ -18,7 +18,7 @@ $( document ).ready(function() {
     };
 
     /* hostname of the api server */
-    var api_root = 'http://localhost:5000'
+    var api_root = 'https://ingage.herokuapp.com'
 
     /* logs out the user on the click of the logout link */
     logoutUser = function(ev){
@@ -293,7 +293,55 @@ $( document ).ready(function() {
 				    });
 				    console.log("this is done now");
 
+                    startDateRangePicker = moment().subtract('days', 29);
+                endDateRangePicker = moment();
+                $('#reportrange').daterangepicker({
+                    startDate: startDateRangePicker,
+                    endDate: endDateRangePicker,
+                    minDate: '01/01/2012',
+                    maxDate: '12/31/2014',
+                    dateLimit: { days: 60 },
+                    showDropdowns: true,
+                    showWeekNumbers: false,
+                    timePicker: false,
+                    timePickerIncrement: 1,
+                    timePicker12Hour: true,
+                    ranges: {
+                    'Today': [moment(), moment()],
+                    'Yesterday': [moment().subtract('days', 1), moment().subtract('days', 1)],
+                    'Last 7 Days': [moment().subtract('days', 6), moment()],
+                    'Last 30 Days': [moment().subtract('days', 29), moment()],
+                    'This Month': [moment().startOf('month'), moment()],
+                    'Last Month': [moment().subtract('month', 1).startOf('month'), moment().subtract('month', 1).endOf('month')],
+                    'This Year': [moment().startOf('year'), moment()]
+                    },
+                    opens: 'left',
+                    format: 'MM/DD/YYYY',
+                    separator: ' to ',
+                    locale: {
+                        applyLabel: 'Submit',
+                        fromLabel: 'From',
+                        toLabel: 'To',
+                        customRangeLabel: 'Custom Range',
+                        daysOfWeek: ['Su', 'Mo', 'Tu', 'We', 'Th', 'Fr','Sa'],
+                        monthNames: ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'],
+                        firstDay: 1
+                    }
+                },
+                function(start, end) {
+                    startDateRangePicker = start;
+                    endDateRangePicker = end;
+                    var field_id = $("li.active").attr("id");
+                    if ($("input[name='form_id']")){
+                        that.changeForm(ev=false, field_id=field_id);
+                    }
+                    $('#reportrange span').html(start.format('MMMM D, YYYY') + ' - ' + end.format('MMMM D, YYYY'));
                 }
+          );
+          //Set the initial state of the picker label
+          $('#reportrange span').html(moment().subtract('days', 29).format('MMMM D, YYYY') + ' - ' + moment().format('MMMM D, YYYY'));
+                }
+                
             });
         }
     });
