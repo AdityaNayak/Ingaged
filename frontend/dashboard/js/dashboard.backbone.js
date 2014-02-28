15cbcfcb3905a67d4bd3e0d1ff11a6a68a83724d
Rishabh Verma <me@rishabhverma.me>
2014-02-28 12:22:50 +0530

    changed api root to staging environment in admin panel and dashboard.

--- frontend/dashboard/js/dashboard.backbone.js
+++ frontend/dashboard/js/dashboard.backbone.js
@@ -31,7 +31,7 @@ $( document ).ready(function() {
     };
 
     /* hostname of the api server */
-    var api_root = 'http://localhost:5000'
+    var api_root = 'https://ingage-staging-1.herokuapp.com'
 
     /* logs out the user on the click of the logout link */
     logoutUser = function(ev){
