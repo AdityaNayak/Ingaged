5483a34a091ca10b66a50c245d90dfdce9afc78f
Rishabh Verma <me@rishabhverma.me>
2014-03-23 02:24:19 +0530

    added price/value comparison score.

--- api/ig_api/feedback/api.py
+++ api/ig_api/feedback/api.py
@@ -111,6 +111,8 @@ form_obj = {
     'customer_details_heading': fields.String,
     'feedback_heading': fields.String,
     'nps_score_heading': fields.String,
+    'price_value_exists': fields.Boolean,
+    'price_value_heading': fields.String,
     'incremental_id': fields.Boolean
 }
 
@@ -139,6 +141,7 @@ feedback_obj = {
 
     # feedback information
     'nps_score': fields.Integer,
+    'price_value_score': fields.Integer,
     'feedback_text': fields.String,
     'received_at': fields.DateTime,
     'customer': fields.Nested(customer_obj),
@@ -188,6 +191,8 @@ class FormList(Resource):
     post_parser.add_argument('description', required=True, type=unicode, location='json')
     post_parser.add_argument('feedback_heading', required=True, type=unicode, location='json')
     post_parser.add_argument('nps_score_heading', required=True, type=unicode, location='json')
+    post_parser.add_argument('price_value_exists', required=True, type=bool, location='json')
+    post_parser.add_argument('price_value_heading', required=False, type=unicode, location='json')
     post_parser.add_argument('customer_details_heading', required=True, type=unicode, location='json')
     post_parser.add_argument('incremental_id', required=True, type=bool, location='json')
     post_parser.add_argument('fields', required=True, type=form_fields, location='json')
@@ -299,6 +304,7 @@ class CustomerFeedback(Resource):
 
     put_parser = reqparse.RequestParser()
     put_parser.add_argument('nps_score', required=True, type=unicode, location='json')
+    put_parser.add_argument('price_value_score', required=False, type=unicode, location='json')
     put_parser.add_argument('feedback_text', required=True, type=unicode, location='json')
     put_parser.add_argument('field_responses', required=True, type=dict, location='json')
     put_parser.add_argument('customer_name', required=False, type=unicode, location='json')
@@ -340,7 +346,7 @@ class CustomerFeedback(Resource):
         # save customer feedback
         try:
             feedback = FeedbackModel.create(args['nps_score'], args['feedback_text'], args['field_responses'], \
-                    instance, customer_details)
+                    instance, customer_details, args['price_value_score'])
         except FeedbackException:
             abort_error(4004)
 
