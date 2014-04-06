706923e4fa075a18e914089802344f8859649eab
Rishabh Verma <me@rishabhverma.me>
2014-04-07 01:08:04 +0530

    added r.js & build.js for optimizing all js files into a single one.

---
+++ frontend/customer/assets/build.js
@@ -0,0 +1,14 @@
+// Filename: build.js
+
+({
+    baseUrl: ".",
+    paths: {
+        'jquery': 'libs/jquery',
+        'underscore': 'libs/underscore',
+        'backbone': 'libs/backbone',
+        'jquery.fullPage': 'libs/jquery.fullPage',
+        'jquery.nouislider': 'libs/jquery.nouislider'
+    },
+    name: "main",
+    out: "main-built.js",
+})
