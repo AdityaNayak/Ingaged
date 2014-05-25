5cfbc9684ecd14dc9d7ec89cb9049f54e629cf7f
Rishabh Verma <me@rishabhverma.me>
2014-05-25 13:37:51 +0530

    endpoint to get nps score graph values.

--- api/ig_api/helpers/__init__.py
+++ api/ig_api/helpers/__init__.py
@@ -89,3 +89,16 @@ def send_trans_email(trans_name, emails, template_vars={}):
     msg = SESMessage(email_details['from'], emails, email_details['subject'])
     msg.html = render_template(email_details['template'], **template_vars)
     msg.send()
+
+
+## Methods to play around with dates
+def iso_year_start(iso_year):
+    "The gregorian calendar date of the first day of the given ISO year"
+    fourth_jan = datetime.date(iso_year, 1, 4)
+    delta = datetime.timedelta(fourth_jan.isoweekday()-1)
+    return fourth_jan - delta 
+
+def iso_to_gregorian(iso_year, iso_week, iso_day):
+    "Gregorian calendar date for the given ISO year, week and day"
+    year_start = iso_year_start(iso_year)
+    return year_start + datetime.timedelta(days=iso_day-1, weeks=iso_week-1)
